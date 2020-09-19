00cecd3e6e707ac98690f0169609c9a4f5a4e063
ufukcanli <iletisim@ufukcanli.com>
2020-09-19 19:43:41 +0300

    Create models.

---
+++ GHFollowers/Models/Follower.swift
@@ -0,0 +1,14 @@
+//
+//  Follower.swift
+//  GHFollowers
+//
+//  Created by Ufuk Canlı on 19.09.2020.
+//  Copyright © 2020 Ufuk Canlı. All rights reserved.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+}
