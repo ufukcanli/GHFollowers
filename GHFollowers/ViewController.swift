5b7fc2ab30ce165a46f5d80fb8ac7316def9be18
ufukcanli <iletisim@ufukcanli.com>
2020-09-19 11:30:18 +0300

    Initial Commit

---
+++ GHFollowers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GHFollowers
+//
+//  Created by Ufuk Canlı on 19.09.2020.
+//  Copyright © 2020 Ufuk Canlı. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
