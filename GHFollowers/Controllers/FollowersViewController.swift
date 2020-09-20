40aadefdfb9ede95a721a80d74cfae4790feaf0d
ufukcanli <iletisim@ufukcanli.com>
2020-09-20 09:23:22 +0300

    Fix navigation bar title.

--- GHFollowers/Controllers/FollowersViewController.swift
+++ GHFollowers/Controllers/FollowersViewController.swift
@@ -29,7 +29,7 @@ class FollowersViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
-        navigationController?.setNavigationBarHidden(true, animated: true)
+        navigationController?.setNavigationBarHidden(false, animated: true)
         navigationController?.navigationBar.prefersLargeTitles = true
     }
 
