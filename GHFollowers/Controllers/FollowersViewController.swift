86e624ec0217008a2f0187423082fad56b1aaf42
ufukcanli <iletisim@ufukcanli.com>
2020-09-20 12:00:29 +0300

    Refactor error messages.

--- GHFollowers/Controllers/FollowersViewController.swift
+++ GHFollowers/Controllers/FollowersViewController.swift
@@ -19,7 +19,7 @@ class FollowersViewController: UIViewController {
         
         NetworkManager.shared.getFollowers(for: username, page: 1) { (followers, errorMessage) in
             guard let followers = followers else {
-                self.presentGFAlertOnMainThread(title: "Bad stuff happened", message: errorMessage!, buttonTitle: "Ok")
+                self.presentGFAlertOnMainThread(title: "Bad stuff happened", message: errorMessage!.rawValue, buttonTitle: "Ok")
                 return
             }
             print(followers.count, followers)
--- GHFollowers/Managers/NetworkManager.swift
+++ GHFollowers/Managers/NetworkManager.swift
@@ -16,27 +16,27 @@ class NetworkManager {
     
     private init() {}
     
-    func getFollowers(for username: String, page: Int, completion: @escaping ([Follower]?, String?) -> Void) {
+    func getFollowers(for username: String, page: Int, completion: @escaping ([Follower]?, ErrorMessage?) -> Void) {
         
         let endpoint = "\(baseURL)/users/\(username)/followers?per_page=100&page=\(page)"
         
         guard let url = URL(string: endpoint) else {
-            completion(nil, "Try another username.")
+            completion(nil, .invalidUsername)
             return
         }
         
         let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
             if let _ = error {
-                completion(nil, "Check your network connection.")
+                completion(nil, .unableToComplete)
             }
             
             guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
-                completion(nil, "Invalid response. Try again.")
+                completion(nil, .invalidResponse)
                 return
             }
             
             guard let data = data else {
-                completion(nil, "Couldn't get the data from the server.")
+                completion(nil, .invalidData)
                 return
             }
             
@@ -46,7 +46,7 @@ class NetworkManager {
                 let followers = try decoder.decode([Follower].self, from: data)
                 completion(followers, nil)
             } catch {
-                completion(nil, "Data received from the server is invalid.")
+                completion(nil, .invalidData)
             }
         }
         
