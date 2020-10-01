d9aad66c0ccb1b429a158144ce7f60d337c75fba
ufukcanli <iletisim@ufukcanli.com>
2020-10-01 15:11:27 +0300

    Some refactoring.

--- GHFollowers/Controllers/FollowersViewController.swift
+++ GHFollowers/Controllers/FollowersViewController.swift
@@ -10,9 +10,7 @@ import UIKit
 
 class FollowersViewController: UIViewController {
     
-    enum Section {
-        case main
-    }
+    enum Section { case main }
         
     var username: String!
     var followers = [Follower]()
@@ -38,33 +36,19 @@ class FollowersViewController: UIViewController {
     }
     
     func fetchFollowers() {
-        NetworkManager.shared.getFollowers(for: username, page: 1) { result in
+        NetworkManager.shared.getFollowers(for: username, page: 1) { [weak self] result in
             switch result {
             case .success(let followers):
-                self.followers = followers
-                self.updateData()
+                self?.followers = followers
+                self?.updateData()
             case .failure(let error):
-                self.presentGFAlertOnMainThread(title: "Bad Stuff Happened", message: error.rawValue, buttonTitle: "Ok")
+                self?.presentGFAlertOnMainThread(title: "Bad Stuff Happened", message: error.rawValue, buttonTitle: "Ok")
             }
         }
     }
-    
-    func createThreeColumnFlowLayout() -> UICollectionViewFlowLayout {
-        let width = view.bounds.width
-        let padding: CGFloat = 12
-        let minimumItemSpacing: CGFloat = 10
-        let availableWidth = width - (padding * 2) - (minimumItemSpacing * 2)
-        let itemWidth = availableWidth / 3
-        
-        let flowLayout = UICollectionViewFlowLayout()
-        flowLayout.sectionInset = UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding)
-        flowLayout.itemSize = CGSize(width: itemWidth, height: itemWidth + 40)
-        
-        return flowLayout
-    }
 
     func configureCollectionView() {
-        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createThreeColumnFlowLayout())
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: UIHelper.createThreeColumnFlowLayout(in: view))
         collectionView.register(FollowerCell.self, forCellWithReuseIdentifier: FollowerCell.reuseIdentifier)
         collectionView.backgroundColor = .systemPink
         view.addSubview(collectionView)
