388f8ce8f4d82a8013fd644c89c6e41d377d90e5
ufukcanli <iletisim@ufukcanli.com>
2020-10-07 15:50:14 +0300

    Some refactoring on UserInfoViewController.

--- GHFollowers/Controllers/UserInfoViewController.swift
+++ GHFollowers/Controllers/UserInfoViewController.swift
@@ -11,23 +11,29 @@ import UIKit
 class UserInfoViewController: UIViewController {
     
     let headerView = UIView()
+    let itemViewOne = UIView()
+    let itemViewTwo = UIView()
     
     var username: String!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .systemBackground
-        
-        let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(dismissViewController))
-        navigationItem.rightBarButtonItem = doneButton
-        
+        configureViewController()
         layoutUI()
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
+        fetchUserInfo()
+    }
+    
+    @objc func dismissViewController() {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func fetchUserInfo() {
         NetworkManager.shared.getUserInfo(for: username) { [weak self] result in
             
             guard let self = self else { return }
@@ -40,12 +46,7 @@ class UserInfoViewController: UIViewController {
             case .failure(let error):
                 self.presentGFAlertOnMainThread(title: "Something went wrong", message: error.rawValue, buttonTitle: "Ok")
             }
-            
         }
-    }
-    
-    @objc func dismissViewController() {
-        dismiss(animated: true, completion: nil)
     }
     
     func add(childViewController: UIViewController, to containerView: UIView) {
@@ -55,16 +56,44 @@ class UserInfoViewController: UIViewController {
         childViewController.didMove(toParent: self)
     }
     
+    func configureViewController() {
+        view.backgroundColor = .systemBackground
+        
+        let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(dismissViewController))
+        navigationItem.rightBarButtonItem = doneButton
+    }
+    
     func layoutUI() {
+        let padding: CGFloat = 20
+        let itemHeight: CGFloat = 140
+        
         view.addSubview(headerView)
+        view.addSubview(itemViewOne)
+        view.addSubview(itemViewTwo)
+        
+        itemViewOne.backgroundColor = .systemPink
+        itemViewTwo.backgroundColor = .systemBlue
         
         headerView.translatesAutoresizingMaskIntoConstraints = false
+        itemViewOne.translatesAutoresizingMaskIntoConstraints = false
+        itemViewTwo.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
             headerView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
             headerView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             headerView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            headerView.heightAnchor.constraint(equalToConstant: 180)
+            headerView.heightAnchor.constraint(equalToConstant: 180),
+            
+            itemViewOne.topAnchor.constraint(equalTo: headerView.bottomAnchor, constant: padding),
+            itemViewOne.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: padding),
+            itemViewOne.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding),
+            itemViewOne.heightAnchor.constraint(equalToConstant: itemHeight),
+            
+            
+            itemViewTwo.topAnchor.constraint(equalTo: itemViewOne.bottomAnchor, constant: padding),
+            itemViewTwo.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: padding),
+            itemViewTwo.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding),
+            itemViewTwo.heightAnchor.constraint(equalToConstant: itemHeight),
         ])
     }
     
